9d3ffab2dc7c646205037c5b3aac42b77229238e
Yuliia <juliamoroz11011@gmail.com>
2022-11-27 17:01:37 +0000

    Merged into one page

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -2,42 +2,95 @@ package com.example.assignment
 
 
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.assignment.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var binding : ActivityMainBinding
+
+    private var sensorManager: SensorManager?= null
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(DashboardFragment())
+        setContentView(R.layout.activity_main)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
+        loadData()
+        resetSteps()
 
-            when(it.itemId){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-                R.id.ic_dashboard -> replaceFragment(DashboardFragment())
-                R.id.ic_settings -> replaceFragment(SettingsFragment())
+    }
 
-                else ->{
-                }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if(stepSensor == null){
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(running) {
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            val tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+            tv_stepsTaken.text = ("$currentSteps")
+
+            val progress_circular = findViewById<com.mikhaellopez.circularprogressbar.CircularProgressBar>(R.id.progress_circular)
+            progress_circular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
             }
-            true
         }
     }
 
-    private fun replaceFragment(fragment : Fragment){
+    private fun resetSteps() {
+        val tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        tv_stepsTaken.setOnClickListener {
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+        tv_stepsTaken.setOnLongClickListener{
+            previousTotalSteps = totalSteps
+            tv_stepsTaken.text = 0.toString()
+            saveData()
+            true
+        }
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
+    }
 
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
 
+    }
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+
+        previousTotalSteps = savedNumber
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
