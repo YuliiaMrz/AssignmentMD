419c59cec883c92917e1c012f87122c782ec4c9c
Yuliia <juliamoroz11011@gmail.com>
2022-12-11 19:17:15 +0000

    Pedometer

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -2,47 +2,61 @@ package com.example.assignment
 
 
 
-import android.annotation.SuppressLint
+
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.example.assignment.databinding.ActivityMainBinding
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.lang.Math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
 
-    private var sensorManager: SensorManager?= null
+    private var magnitudePreviousStep = 0.0
+    private var sensorManager: SensorManager? = null
     private var running = false
     private var totalSteps = 0f
     private var previousTotalSteps = 0f
+    private val ACTIVITY_RECOGNITION_REQUEST_CODE: Int = 100
     var num = 0
+    var num2 = 0
     var startPoint = 0
     var endPoint = 0
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (isPermissionGranted()) {
+            requestPermission()
+        }
         loadData()
         resetSteps()
-
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
 
         val age1 = findViewById<Button>(R.id.age1)
         val age2 = findViewById<Button>(R.id.age2)
         var textView = findViewById<TextView>(R.id.age)
         age1.setOnClickListener {
-            num--
-            textView.text = num.toString()
+            if (num <= 0) {
+                num == 0
+                textView.text = num.toString()
+            } else {
+                num--
+                textView.text = num.toString()
+            }
         }
         age2.setOnClickListener {
             num++
@@ -52,62 +66,105 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val weight2 = findViewById<Button>(R.id.weight2)
         var textView2 = findViewById<TextView>(R.id.weight)
         weight1.setOnClickListener {
-            num--
-            textView2.text = num.toString()
+            if (num2 <= 0) {
+                num2 == 0
+                textView2.text = num2.toString()
+            } else {
+                num2--
+                textView2.text = num2.toString()
+            }
         }
         weight2.setOnClickListener {
-            num++
-            textView2.text = num.toString()
+            num2++
+            textView2.text = num2.toString()
         }
 
         val volumeSeekBar = findViewById<SeekBar>(R.id.seek_bar)
         val volume = findViewById<TextView>(R.id.height_value)
-        volumeSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        volumeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 volume.text = progress.toString()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
                 if (seekBar != null) {
+                    seekBar.setMax(251)
                     startPoint = seekBar.progress
                 }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 if (seekBar != null) {
+
                     endPoint = seekBar.progress
                 }
-                Toast.makeText(this@MainActivity, "changed height", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "changed height", Toast.LENGTH_LONG).show()
             }
         })
     }
 
 
+
     override fun onResume() {
         super.onResume()
-        running = true
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        val detectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-        if(stepSensor == null){
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        } else {
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        when {
+            countSensor != null -> {
+                sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI)
+            }
+            detectorSensor != null -> {
+                sensorManager.registerListener(this, detectorSensor, SensorManager.SENSOR_DELAY_UI)
+            }
+            accelerometer != null -> {
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+            }
+            else -> {
+                Toast.makeText(this, "Your device is not compatible", Toast.LENGTH_LONG).show()
+            }
         }
 
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if(running) {
-            totalSteps = event!!.values[0]
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            val tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-            tv_stepsTaken.text = ("$currentSteps")
+        val steptaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        val cirbar = findViewById<com.mikhaellopez.circularprogressbar.CircularProgressBar>(R.id.progress_circular)
+        if(event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val xaccel: Float = event.values[0]
+            val yaccel: Float = event.values[1]
+            val zaccel: Float = event.values[2]
+            val magnitude: Double = sqrt((xaccel * xaccel + yaccel * yaccel + zaccel * zaccel).toDouble())
 
-            val progress_circular = findViewById<com.mikhaellopez.circularprogressbar.CircularProgressBar>(R.id.progress_circular)
-            progress_circular.apply {
-                setProgressWithAnimation(currentSteps.toFloat())
+            val magnitudeDelta: Double = magnitude - magnitudePreviousStep
+            magnitudePreviousStep = magnitude
+
+            if(magnitudeDelta > 6) {
+                totalSteps++
+            }
+            val step: Int = totalSteps.toInt()
+            steptaken.text = step.toString()
+            cirbar.apply {
+                setProgressWithAnimation(step.toFloat())
+            }
+
+        } else {
+            if(running) {
+                totalSteps = event!!.values[0]
+                val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+                steptaken.text = currentSteps.toString()
+                cirbar.apply {
+                    setProgressWithAnimation(currentSteps.toFloat())
+                }
             }
         }
+
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //
     }
 
     private fun resetSteps() {
@@ -134,13 +191,33 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private fun loadData() {
         val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val savedNumber = sharedPreferences.getFloat("key1", 0f)
-
+        Log.d("MainActivity", "$savedNumber")
         previousTotalSteps = savedNumber
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION), ACTIVITY_RECOGNITION_REQUEST_CODE)
+        }
     }
 
+    private fun isPermissionGranted(): Boolean {
+    return ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            ACTIVITY_RECOGNITION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+            }
+        }
+    }
 }
